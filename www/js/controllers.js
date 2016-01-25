778be36a2fffd43edccb1e011bde3445d4e59136
Roland Oviol <rolandoviol@gmail.com>
2016-01-25 09:51:38 -0400

    inicial

---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+
+})
+
+.controller('PlaylistsCtrl', ['$scope', '$http',
+  function ($scope, $http)  {
+  $http.get('http://www.racoya.com/blog/wp-json/wp/v2/posts').success(function(data) {
+    $scope.playlists = data;
+    console.log("pasa");
+  });
+  /*
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];*/
+}])
+
+.controller('PlaylistCtrl', ['$scope', '$stateParams', '$http',
+  function($scope, $stateParams, $http) {
+    $scope.playlistid=$stateParams.playlistId;
+
+    $http.get('http://www.racoya.com/blog/wp-json/wp/v2/posts/'+$scope.playlistid).success(function(data) {
+      $scope.playlista = data;
+      console.log("pasa2");
+    });
+}]);
