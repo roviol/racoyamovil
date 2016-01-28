5540a60b28b43e9b4053873a36535bae19e7f0f9
Roland Oviol <rolandoviol@gmail.com>
2016-01-28 12:03:00 -0400

    view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,8 +42,28 @@ angular.module('starter.controllers', [])
 
 .controller('BrowserCtrl',  ['$scope', '$http',
   function ($scope,$http)  {
+     $http.get('js/unidades.json').success(function(data) {
+        $scope.unidades = data;
+        $scope.unisel = $scope.unidades[0];
+        $scope.uniseldest = $scope.unidades[1];
+     });    
+     $http.get('js/densidades.json').success(function(data) {
+        $scope.densidades = data;
+        $scope.densel = $scope.densidades[0];
+     });    
      $http.get('js/equivalencias.json').success(function(data) {
         $scope.equivalencias = data;
      });    
-}]);
+     $scope.resultado = 0;
+     $scope.cantidad = 1;
+     console.log("pasa4a");
 
+     $scope.update = function() {
+         $scope.resultado = $scope.cantidad * $scope.densel.densidad;
+         factor=$scope.equivalencias[$scope.unisel.id][$scope.uniseldest.id];
+         console.log($scope.unisel.id);
+         console.log($scope.uniseldest.id);
+         console.log(factor);
+     }
+}]);
+ 
